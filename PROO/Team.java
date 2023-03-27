513859abde1fbe53b6cba30c8ba9cac0815fbe69
OstrichDowneyJr <wiewior.niewystraszny@gmail.com>
2023-03-27 21:46:54 +0200

    Poprawa struktury

--- PROO/Team.java
+++
@@ -1,9 +0,0 @@
-public class Team{
-    private String name;
-    public final String members[];
-    public Team(){
-        name = "BestTeam";
-        members = new String[] {"Ja","On"};
-    }
-    
-}
