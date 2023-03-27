7ce271900f907a076266634dcc295b28e2e596ad
OstrichDowneyJr <jan.burbicki@gmail.com>
2023-03-27 13:27:44 +0200

    Add files via upload

---
+++ PROO/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+          System.out.println("Hello UwU");
+          Team myTeam = new Team();    
+          for(int i=0; i<1;i++){
+            System.out.println(myTeam.members[i]);
+          }
+          
+    } 
+}
---
+++ PROO/Team.java
@@ -0,0 +1,9 @@
+public class Team{
+    private String name;
+    public final String members[];
+    public Team(){
+        name = "BestTeam";
+        members = new String[] {"Ja","On"};
+    }
+    
+}
